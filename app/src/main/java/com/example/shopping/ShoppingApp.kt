89dfd6b124d5edf42d1bef2e18d6a7e9539e8170
Atabek <atabekmur@gmail.com>
2018-11-24 20:49:54 +1100

    Fixes daggers AppComponent

--- app/src/main/java/com/example/shopping/ShoppingApp.kt
+++ app/src/main/java/com/example/shopping/ShoppingApp.kt
@@ -3,12 +3,14 @@ package com.example.shopping
 import android.app.Application
 import com.example.shopping.di.component.AppComponent
 import com.example.shopping.di.component.DaggerAppComponent
+import com.example.shopping.di.module.AppModule
 
 class ShoppingApp : Application() {
 
     val component: AppComponent by lazy {
         DaggerAppComponent
             .builder()
+            .appModule(AppModule(this))
             .build()
     }
 
